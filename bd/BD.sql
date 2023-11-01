5a4b20eea56a39b83d08d96c8d9a57c09ae89272
Angelika <angelika_romanovskaya_2002@mail.ru>
2023-11-01 15:59:40 +0300

    CRUD manager

--- bd/BD.sql
+++ bd/BD.sql
@@ -4,7 +4,16 @@ DELIMITER $$
 CREATE PROCEDURE addClient(in login varchar(45), in password varchar(255), in name varchar(50), in surname varchar(50), in patronymic varchar(50), in email varchar(50), in phone varchar(20), in iv varchar(255))
 BEGIN
 	insert into user(login, password, id_role, iv) value(login, password, 3, iv);
-    insert into client(name, surname, patronymic, email, phone, id_user) value(name, surname, patronymic, email, phone, (select id from user where login = login and password = password));
+    insert into client(name, surname, patronymic, email, phone, id_user) value(name, surname, patronymic, email, phone, (select id from user where user.login = login and user.password = password));
 end;
 
 drop procedure addClient;
+
+DELIMITER $$
+CREATE PROCEDURE addManager(in login varchar(45), in password varchar(255), in name varchar(50), in surname varchar(50), in phone varchar(20), in iv varchar(255))
+BEGIN
+	insert into user(login, password, id_role, iv) value(login, password, 2, iv);
+    insert into manager(name, surname, phone, id_user) value(name, surname, phone, (select id from user where user.login = login and user.password = password));
+end;
+
+drop procedure addManager;
--- server/index.js
+++ server/index.js
@@ -25,21 +25,20 @@ app.post('/login', (req, res)=>{
     const {password, login} = req.body;
     db.query("select * from user", (err, result)=>{
         if(err) {
-            console.log(err);
+            res.send({status: "error", error: err});
         } else{
-            result.forEach(elem=>{
-                if(decrypt({password: elem.password, iv: elem.iv}) === password && elem.login === login){
-                    db.query("select role from role where id = ?", [elem.id_role], (err, result)=>{
-                        if(err){
-                            console.log(err);
-                        } else{
-                            res.send({status: "success", role: result[0].role})
-                        }
-                    })
-                } else{
+            let user =  result.filter(elem=>decrypt({password: elem.password, iv: elem.iv}) === password && elem.login === login);
+            if(user.length !== 0){
+                db.query("select role.role from role where role.id = ?", [user[0].id_role], (err, result)=>{
+                    if(err){
+                        res.send({status: "error", error: err});
+                    } else{
+                        res.send({status: "success", role: result[0].role})
+                    }
+                });
+            } else{
                     res.send({status: "not found"})
-                }
-            })
+            }
         }
     });
 });
@@ -49,12 +48,54 @@ app.post('/registration', (req, res)=>{
     const hachedPassword = encrypt(password);
     db.query("call addClient(?,?,?,?,?,?,?,?)", [login, hachedPassword.password, name, surname, patronymic, email, phone, hachedPassword.iv], (err, result)=>{
         if(err){
-            console.log(err);
+            if(err.code === "ER_DUP_ENTRY") {
+                res.send({status: "duplicate"});
+            } else{
+                res.send({status: "error", error: err});
+            }
         } else{
-            res.send("Success");
+            res.send({status: "success"});
         }
     });
 });
+
+app.post('/addmanager', (req, res)=>{
+    const {password, login, name, surname, phone} = req.body;
+    const hachedPassword = encrypt(password);
+    db.query("call addManager(?,?,?,?,?,?)", [login, hachedPassword.password, name, surname, phone, hachedPassword.iv], (err, result)=>{
+        if(err){
+            if(err.code === "ER_DUP_ENTRY") {
+                res.send({status: "duplicate"});
+            } else{
+                res.send({status: "error", error: err});
+            }
+        } else{
+            res.send({status: "success"});
+        }
+    });
+});
+
+app.get('/getmanagers', (req,res)=>{
+    db.query("select * from manager", (err, result)=>{
+        console.log(result);
+        if(err){
+            res.send({status: "error", error: err});
+        } else{
+            res.send({status: "success", managers: result})
+        }
+    })
+});
+
+app.post('/deleteManager', (req,res)=>{
+    const {id} = req.body;
+    db.query("delete user from user join manager on user.id = manager.id_user where manager.id = ?", [id], (err, result)=>{
+        if(err){
+            res.send({status: "error", error: err});
+        } else{
+            res.send({status: "success"})
+        }
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log("Server is running");
